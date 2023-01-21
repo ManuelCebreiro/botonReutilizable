4e443b54d0df93d5037ed1286d33e22ecaf6515c
Manuel <cebreirom@gmail.com>
2023-01-21 02:10:48 +0100

    first commit

--- src/components/Boton.tsx
+++ src/components/Boton.tsx
@@ -1,18 +1,43 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+
+export interface Props {
+  primary: boolean
+  // secondary: boolean
+  // terciary: boolean
+  // noBG: boolean
+  
+}
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Button = styled.button`
-    background-color: red;
-`
+  width: 126px;
+  height: 48px;
+  top: 20px;
+  left: 20px;
+  border-radius: 8px;
+  padding: 12px 16px 12px 16px;
+  font-size: 16px;
+  background: ${(props: Props) => (props.primary ? "#034426" : "#3c7f8b")};
+`;
 
 
-
-function Boton() {
+function Boton(props) {
   return (
-    <>
-    <Button>Hola</Button>
-    </>
-  )
+    <Container>
+      <Button primary>Button label</Button>
+    </Container>
+  );
 }
 
-export default Boton
+export default Boton;
+
+
+
+
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import Boton from './components/Boton'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Boton from "./components/Boton";
+import styled from "styled-components";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-<Boton/>
-  </React.StrictMode>,
-)
+      <Boton />
+  </React.StrictMode>
+);
